4bc666b5cc976aeb05a441e8252c7b0a73afa985
Shurik <86realmadrid86@gmail.com>
2021-10-26 19:13:09 +0300

    started developing the web-application on next.js + react
    -first steps
    -renamed index.js and _app.js in tsx files
    -created _document.tsx
    -connected google fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, {DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript} from "next/document";
+
+
+
+export default class MyDocument extends Document {
+
+    // метод getInitialProps возвращает initialProps из исходного документа в наш кастомный документ
+    static async getInitialProps(ctx:DocumentContext): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(ctx);
+        return {...initialProps};
+    }
+
+    render(): JSX.Element {
+        return (
+            <Html lang="ru">
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
