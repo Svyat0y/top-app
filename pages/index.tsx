203053287562a794e5bff7b9fd58bc794290e2fe
Shurik <86realmadrid86@gmail.com>
2021-10-31 11:09:02 +0200

    added Layout, Header, Sidebar, Footer components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,33 @@
 import {Htag, Button, Ptag, Tag, Rating} from '../components';
 import {useState}                        from 'react';
+import {Layout}                          from '../layout/Layout';
 
 
 export default function Home(): JSX.Element {
 
-        const [rating, setRating] = useState<number>(1);
+    const [rating, setRating] = useState<number>(0);
 
-        return (
-            <div>
-                    <Htag tag='h1'>Заголовок</Htag>
+    return (
+        <>
+            <Htag tag='h1'>Заголовок</Htag>
 
-                    <Button appearance='primary' arrow='right'>Кнопка</Button>
-                    <Button appearance='ghost' arrow='down'>Кнопка</Button>
-                    <Button appearance='ghost' arrow='down'>Кнопка</Button>
-                    <Button appearance='ghost' arrow='down'>Кнопка</Button>
-                    <Button appearance='ghost' arrow='down'>Кнопка</Button>
+            <Button appearance='primary' arrow='right'>Кнопка</Button>
+            <Button appearance='ghost' arrow='down'>Кнопка</Button>
+            <Button appearance='ghost' arrow='down'>Кнопка</Button>
+            <Button appearance='ghost' arrow='down'>Кнопка</Button>
+            <Button appearance='ghost' arrow='down'>Кнопка</Button>
 
-                    <Ptag size='l'>параграф</Ptag>
-                    <Ptag size='s'>параграф</Ptag>
-                    <Ptag>параграф</Ptag>
+            <Ptag size='l'>параграф</Ptag>
+            <Ptag size='s'>параграф</Ptag>
+            <Ptag>параграф</Ptag>
 
-                    <Tag>Tag1</Tag>
-                    <Tag size='s' color='gray'>Tag2</Tag>
-                    <Tag color='green'>Tag3</Tag>
-                    <Tag color='primary'>Tag4</Tag>
-                    <Tag href='https://google.com.ua' color='red'>Tag5</Tag>
+            <Tag>Tag1</Tag>
+            <Tag size='s' color='gray'>Tag2</Tag>
+            <Tag color='green'>Tag3</Tag>
+            <Tag color='primary'>Tag4</Tag>
+            <Tag href='https://google.com.ua' color='red'>Tag5</Tag>
 
-                    <Rating rating={rating} isEditable={true} setRating={setRating}/>
-            </div>
-        );
+            <Rating rating={rating} isEditable={true} setRating={setRating}/>
+        </>
+    );
 }
