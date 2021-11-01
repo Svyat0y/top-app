e217a1746970d8f3eeaa695ab6ce2e5af7319afc
Shurik <86realmadrid86@gmail.com>
2021-11-01 21:41:23 +0200

    implemented SSR for courses page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,44 @@
-import {Htag, Button, Ptag, Tag, Rating} from '../components';
-import {useState}                        from 'react';
-import {withLayout}                      from '../hoc/withLayout';
+import {useState}       from 'react';
+import {withLayout}     from '../hoc/withLayout';
+import {GetStaticProps} from 'next';
+import {MenuItem}       from '../interfaces/menu.interface';
+import axios            from 'axios';
 
 
-const Home = (): JSX.Element => {
+const Home = ({menu}: HomeProps): JSX.Element => {
 
     const [rating, setRating] = useState<number>(0);
 
     return (
         <>
-            <Htag tag='h1'>Заголовок</Htag>
-
-            <Button appearance='primary' arrow='right'>Кнопка</Button>
-            <Button appearance='ghost' arrow='down'>Кнопка</Button>
-            <Button appearance='ghost' arrow='down'>Кнопка</Button>
-            <Button appearance='ghost' arrow='down'>Кнопка</Button>
-            <Button appearance='ghost' arrow='down'>Кнопка</Button>
-
-            <Ptag size='l'>параграф</Ptag>
-            <Ptag size='s'>параграф</Ptag>
-            <Ptag>параграф</Ptag>
-
-            <Tag>Tag1</Tag>
-            <Tag size='s' color='gray'>Tag2</Tag>
-            <Tag color='green'>Tag3</Tag>
-            <Tag color='primary'>Tag4</Tag>
-            <Tag href='https://google.com.ua' color='red'>Tag5</Tag>
-
-            <Rating rating={rating} isEditable={true} setRating={setRating}/>
+            <ul>
+                {menu.map((m) =>
+                    <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+                )}
+            </ul>
         </>
     );
 };
 
 export default withLayout(Home);
+
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const firstCategory = 0;
+
+    const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+        firstCategory
+    });
+
+    return {
+        props: {
+            menu,
+            firstCategory
+        }
+    };
+};
+
+interface HomeProps extends Record<string, unknown> {
+    menu: MenuItem[]
+    firstCategory: number
+}
